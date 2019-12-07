3d2833ca0f3f552029c7a3fbce8a87ab3e1f569d
Stephen von Schmidt-Pauli <stephenvsp@gmail.com>
2019-12-07 12:54:27 -0500

    problem 1 part 2 complete

--- p1/p1.py
+++ p1/p1.py
@@ -1,5 +1,10 @@
 import math
 
+
+def calculateFuel(mass):
+    return (math.floor(mass / 3)) - 2
+
+
 inputFile = open("p1_input.py", "r")
 outputFile = open("p1_output.py", "w")
 
@@ -7,8 +12,16 @@ sum = 0
 
 for i in inputFile:
     mass = int(i)
-    fuel = (math.floor(mass / 3)) - 2
-    sum += fuel
+    fuel = calculateFuel(mass)
+    fuelTotal = fuel
+
+    while(fuel > 0):
+        fuel = calculateFuel(fuel)
+        if (fuel > 0):
+            fuelTotal += fuel
+
+    sum += fuelTotal
+
 
 outputFile.write(str(sum))
 
