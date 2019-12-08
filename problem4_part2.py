61870793e3d7653cb18b449f55941840d53cb764
Stephen vSP <steve96@vt.edu>
2019-12-07 21:26:07 -0500

    solved problem 4 part 2

--- problem4_part2.py
+++ problem4_part2.py
@@ -5,10 +5,19 @@ upperLimit = 893698
 def containsTwoAdjacentDigits(input):
     input = str(input)
 
-    for i in range(0, len(input) - 1):
-        if (input[i] == input[i + 1]):
+    numDict = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
+
+    for c in input:
+        numDict[c] = numDict[c] + 1
+
+    for item in numDict.items():
+        key, value = item
+
+        if (value == 2):
             return True
+    
     return False
+    
 
 
 def ascendingNumber(input):
@@ -22,9 +31,8 @@ def ascendingNumber(input):
 
 count = 0
 
-# for i in range(lowerLimit, upperLimit):
-#    if (containsTwoAdjacentDigits(i) and ascendingNumber(i)):
-#        count += 1
+for i in range(lowerLimit, upperLimit):
+    if (containsTwoAdjacentDigits(i) and ascendingNumber(i)):
+        count += 1
 
-print(containsTwoAdjacentDigits(123444))
-print(containsTwoAdjacentDigits(111122))
+print(count)
