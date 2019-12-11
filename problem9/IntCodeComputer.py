a1173c7e8ac335555c56fc609f80cbe89db19546
Stephen von Schmidt-Pauli <stephenvsp@gmail.com>
2019-12-10 21:49:24 -0500

    a lot of refactoring

--- problem9/IntCodeComputer.py
+++ problem9/IntCodeComputer.py
@@ -1,4 +1,5 @@
 def run_program(int_code, inputs=[]):
+
     output = []
     relative_base = 0
     pc = 0
@@ -34,137 +35,117 @@ def run_program(int_code, inputs=[]):
         elif mode == 2:
             new_location = int_code[pc] + relative_base
 
-        int_code = grow_memory_if_needed(new_location)
+        grow_memory_if_needed(new_location)
         return new_location
 
     def grow_memory_if_needed(new_position):
         last_space = len(int_code) - 1
 
-        if (new_position > last_space):
-            difference = new_position - last_space
-
-            new_memory = [0 for i in range(0, difference)]
-            int_code.extend(new_memory)
-
-        return int_code
+        int_code.extend(
+            [0 for i in range(last_space, new_position)])
 
     while pc < len(int_code):
 
-        third_param_mode, second_param_mode, first_param_mode, operation = parse_opcode(
+        third_mode, second_mode, first_mode, operation = parse_opcode(
             int_code[pc])
 
         # 1 - addition, 2 - multiplication
         if operation == 1 or operation == 2:
-            location_one = get_location(pc + 1, first_param_mode)
+            val1 = get_value(pc + 1, first_mode)
+            val2 = get_value(pc + 2, second_mode)
 
-            location_two = get_location(pc + 2, second_param_mode)
-
-            output_position = get_location(pc + 3, third_param_mode)
-
-            val1 = int_code[location_one]
-            val2 = int_code[location_two]
+            output_position = get_location(pc + 3, third_mode)
 
             result = val1 + val2 if operation == 1 else val1 * val2
 
             int_code[output_position] = result
 
-            pc += 4
+            increment = 4
 
         # 3 - read from input and write to position
         elif (operation == 3):
 
-            location_one = get_location(pc + 1, first_param_mode)
+            location_one = get_location(pc + 1, first_mode)
 
-            inputVal = int(inputs.pop(0))
+            int_code[location_one] = int(inputs.pop(0))
 
-            int_code[location_one] = inputVal
-
-            pc += 2
+            increment = 2
 
         # 4 - print out position
         elif (operation == 4):
-            location_one = get_location(pc + 1, first_param_mode)
-
-            val1 = int_code[location_one]
+            val1 = get_value(pc + 1, first_mode)
 
             output.append(val1)
 
-            pc += 2
+            increment = 2
 
         # 5 - jump if true
         elif (operation == 5):
-            location_one = get_location(pc + 1, first_param_mode)
-            location_two = get_location(pc + 2, second_param_mode)
-
-            val1 = int_code[location_one]
-            val2 = int_code[location_two]
+            val1 = get_value(pc + 1, first_mode)
+            val2 = get_value(pc + 2, second_mode)
 
             if (val1 != 0):
+                increment = 0
                 pc = val2
             else:
-                pc += 3
+                increment = 3
 
         # 6 - jump if false
         elif (operation == 6):
-            location_one = get_location(pc + 1, first_param_mode)
-            location_two = get_location(pc + 2, second_param_mode)
 
-            val1 = int_code[location_one]
-            val2 = int_code[location_two]
+            val1 = get_value(pc + 1, first_mode)
+            val2 = get_value(pc + 2, second_mode)
 
             if val1 == 0:
+                increment = 0
                 pc = val2
             else:
-                pc += 3
+                increment = 3
 
         # 7 - less than
         elif (operation == 7):
-            location_one = get_location(pc + 1, first_param_mode)
+            val1 = get_value(pc + 1, first_mode)
+            val2 = get_value(pc + 2, second_mode)
 
-            location_two = get_location(pc + 2, second_param_mode)
-
-            output_position = get_location(pc + 3, third_param_mode)
-
-            val1 = int_code[location_one]
-            val2 = int_code[location_two]
+            output_position = get_location(pc + 3, third_mode)
 
             if (val1 < val2):
                 int_code[output_position] = 1
             else:
                 int_code[output_position] = 0
 
-            pc += 4
+            increment = 4
 
         # 8 - equals
         elif (operation == 8):
-            location_one = get_location(pc + 1, first_param_mode)
+            val1 = get_value(pc + 1, first_mode)
+            val2 = get_value(pc + 2, second_mode)
 
-            location_two = get_location(pc + 2, second_param_mode)
-
-            output_position = get_location(pc + 3, third_param_mode)
-
-            val1 = int_code[location_one]
-            val2 = int_code[location_two]
+            output_position = get_location(pc + 3, third_mode)
 
             if (val1 == val2):
                 int_code[output_position] = 1
             else:
                 int_code[output_position] = 0
 
-            pc += 4
+            increment = 4
 
         # 9 - change relative base
         elif (operation == 9):
-            location_one = get_location(pc + 1, first_param_mode)
-
-            val1 = int_code[location_one]
+            val1 = get_value(pc + 1, first_mode)
 
             relative_base += val1
 
-            pc += 2
+            increment = 2
 
-        # if operation is 99 no need to anything just exit
-        if (operation == 99):
+        #99 - halt
+        elif (operation == 99):
             break
+
+        else:
+            print('you dont fucked up')
+            break
+
+        pc += increment
 
     return output
