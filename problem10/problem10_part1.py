6a8babe355f632291a058d7c97022ecaf023a23f
Stephen von Schmidt-Pauli <stephenvsp@gmail.com>
2019-12-11 21:56:26 -0500

    got problem 10 part 1

--- problem10/problem10_part1.py
+++ problem10/problem10_part1.py
@@ -1,10 +1,10 @@
 import math
 
-with open('problem10_input.txt') as file:
-    asteroid_map = file.readlines()
-
 # strip \n's out of strings
-asteroid_map = [asteroid.strip() for asteroid in asteroid_map]
+asteroid_map = ['###..#########.#####.', '.####.#####..####.#.#', '.###.#.#.#####.##..##', '##.####.#.###########', '###...#.####.#.#.####', '#.##..###.########...', '#.#######.##.#######.', '.#..#.#..###...####.#', '#######.##.##.###..##', '#.#......#....#.#.#..',
+                '######.###.#.#.##...#', '####.#...#.#######.#.', '.######.#####.#######', '##.##.##.#####.##.#.#', '###.#######..##.#....', '###.##.##..##.#####.#', '##.########.#.#.#####', '.##....##..###.#...#.', '#..#.####.######..###', '..#.####.############', '..##...###..#########']
+
+# asteroid_map = ['.#..#', '.....', '#####', '....#', '...##']
 
 max_x = len(asteroid_map[0])
 max_y = len(asteroid_map)
@@ -13,42 +13,135 @@ max_y = len(asteroid_map)
 asteroid_map = [[asteroid_map[y][x]
                  for y in range(0, max_y)] for x in range(0, max_x)]
 
-max_asteroids_seen = 0
-max_asteroid = -1, -1
-
-max_degrees = 180
-increment = 1
-
 slopes = []
+slopeDict = {}
 
-for degree in range(0, max_degrees, 1):
-    radians = degree * math.pi / 180
+for x in range(1, max_x):
+    for y in range(1, max_y):
+        if (x / y) not in slopeDict.keys():
+            slopes.append((x, y))
+            slopes.append((-x, y))
+            slopeDict[x / y] = True
 
-    x = math.cos(radians)
-    y = math.sin(radians)
 
-    if degree == 90:
-        slopes.append('UNDEFINED')
-    elif degree == 270:
-        slopes.append('UNDEFINED')
-    else:
-        slopes.append(y / x)
+def is_inbounds(my_x, my_y):
 
-print(slopes)
+    if (my_x < 0 or my_x >= max_x):
+        return False
+
+    if (my_y < 0 or my_y >= max_y):
+        return False
+
+    return True
 
 
 def search_for_asteroids(x, y):
-    return 0
+    count = 0
+
+    # up
+    curr_x = x
+    curr_y = y + 1
+    while (is_inbounds(curr_x, curr_y)):
+        if asteroid_map[curr_x][curr_y] == '#':
+            count += 1
+            break
+        curr_y += 1
+
+    # down
+    curr_x = x
+    curr_y = y - 1
+    while (is_inbounds(curr_x, curr_y)):
+        if asteroid_map[curr_x][curr_y] == '#':
+            count += 1
+            break
+        curr_y -= 1
+
+    # left
+    curr_x = x - 1
+    curr_y = y
+    while (is_inbounds(curr_x, curr_y)):
+        if asteroid_map[curr_x][curr_y] == '#':
+            count += 1
+            break
+        curr_x -= 1
+
+    # right
+    curr_x = x + 1
+    curr_y = y
+    while (is_inbounds(curr_x, curr_y)):
+        if asteroid_map[curr_x][curr_y] == '#':
+            count += 1
+            break
+        curr_x += 1
+
+    for slope in slopes:
+        run = slope[0]
+        rise = slope[1]
+
+        # slope is positive
+        if rise / run > 0:
+
+            # go up the slope
+            curr_x = x + run
+            curr_y = y + rise
+
+            while(is_inbounds(curr_x, curr_y)):
+                if asteroid_map[curr_x][curr_y] == '#':
+                    count += 1
+                    break
+                curr_x += run
+                curr_y += rise
+
+            # go down the slope
+            curr_x = x - run
+            curr_y = y - rise
+
+            while(is_inbounds(curr_x, curr_y)):
+                if asteroid_map[curr_x][curr_y] == '#':
+                    count += 1
+                    break
+                curr_x -= run
+                curr_y -= rise
+
+        # slope is negative
+        else:
+            # go up the slope
+            curr_x = x + run
+            curr_y = y + rise
+
+            while(is_inbounds(curr_x, curr_y)):
+                if asteroid_map[curr_x][curr_y] == '#':
+                    count += 1
+                    break
+                curr_x += run
+                curr_y += rise
+
+            # go up the slope
+            curr_x = x - run
+            curr_y = y - rise
+
+            while(is_inbounds(curr_x, curr_y)):
+                if asteroid_map[curr_x][curr_y] == '#':
+                    count += 1
+                    break
+                curr_x -= run
+                curr_y -= rise
+
+    return count
 
 
-# for y in range(0, max_y):
-    # for x in range(0, max_x):
+max_asteroids_seen = 0
+max_asteroid = -1, -1
 
-    #     if asteroid_map[x][y] == '#':
-    #         asteroids_seen = search_for_asteroids(x, y)
+for y in range(0, max_y):
+    for x in range(0, max_x):
 
-    #         if (asteroids_seen > max_asteroids_seen):
-    #             max_asteroids = x, y
+        if asteroid_map[x][y] == '#':
+            asteroids_seen = search_for_asteroids(x, y)
 
+            if (asteroids_seen > max_asteroids_seen):
+                max_asteroids_seen = asteroids_seen
+                max_asteroid = x, y
 
-print(search_for_asteroids(0, 0))
+print(max_asteroids_seen)
+print(max_asteroid)
