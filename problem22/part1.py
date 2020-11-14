8946b3f6fc4d43f0a47c5215b78843cdebf74e2c
stephenvsp <stephenvsp@gmail.com>
2020-11-14 11:47:58 -0500

    refactor part 1 to do it the right way

--- problem22/part1.py
+++ problem22/part1.py
@@ -1,36 +1,28 @@
 
-def shuffle(deck, sequence): 
+def shuffle(deck_size, sequence, offset = 0, increment = 1): 
 
-    deck_size = len(deck)
-
-    def deal_into_new_stack(deck):
-        deck.reverse()
-        return deck
-
-    def deal_with_increment(deck, increment):
-        new_deck = [None] * deck_size
-        pos = 0
-        for i in range(0, deck_size):
-            new_deck[pos % deck_size] = deck[i]
-
-            pos = pos + increment
-        return new_deck
-
-    def cut(deck, amount):
-        cut = deck[:amount]
-        rest = deck[amount:]
-        return rest + cut
+    def mod_inverse(n):
+        return pow(n, deck_size - 2, deck_size)
 
     for shuffle in sequence:
         if "deal into new stack" in shuffle:
-            deck = deal_into_new_stack(deck)
-        
+            increment *= -1
+            offset += increment
+
         elif "deal with increment" in shuffle:
-            increment = int(shuffle.split()[-1])
-            deck = deal_with_increment(deck, increment)
+            new_increment = int(shuffle.split()[-1])
+
+            inverse = mod_inverse(new_increment)
+
+            increment *= inverse
 
         elif "cut" in shuffle:
             amount = int(shuffle.split()[-1])
-            deck = cut(deck, amount)
-    
-    return deck
+
+            offset += increment * amount
+
+        offset = pow(offset, 1, deck_size)
+        increment = pow(increment, 1, deck_size)
+
+    return (offset, increment)
+
--- problem22/tests.py
+++ problem22/tests.py
@@ -3,102 +3,26 @@ from part1 import shuffle
 
 class TestProblem22(unittest.TestCase):
 
-    def test_deal_into_new_stack(self):
-        deck = list(range(0, 10))
-
-        sequence = ['deal into new stack']
-
-        expected_shuffle = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-        actual_shuffle = shuffle(deck, sequence)
-
-        self.assertEqual(actual_shuffle, expected_shuffle)
-
-    def test_cut_N_cards(self):
-
-        deck = list(range(0, 10))
-
-        sequence = ['cut 3']
-
-        expected_shuffle = [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
-        actual_shuffle = shuffle(deck, sequence)
-
-        self.assertEqual(actual_shuffle, expected_shuffle)
-
-    def test_cut_negative_N_cards(self):
-
-        deck = list(range(0, 10))
-
-        sequence = ['cut -4']
-
-        expected_shuffle = [6, 7, 8, 9, 0, 1, 2, 3, 4, 5]
-        actual_shuffle = shuffle(deck, sequence)
-
-        self.assertEqual(actual_shuffle, expected_shuffle)
-
-    def test_increment_N(self):
-
-        deck = list(range(0, 10))
-
-        sequence = ['deal with increment 3']
-
-        expected_shuffle = [0, 7, 4,1,8,5,2,9,6,3]
-        actual_shuffle = shuffle(deck, sequence)
-
-        self.assertEqual(actual_shuffle, expected_shuffle)
-
-    def test_given_one(self):
-
-        deck = list(range(0, 10))
-
-        sequence = ['deal with increment 7','deal into new stack','deal into new stack']
-
-        expected_shuffle = [0,3,6,9,2,5,8,1,4,7]
-        actual_shuffle = shuffle(deck, sequence)
-        self.assertEqual(actual_shuffle, expected_shuffle)
-
-    def test_given_two(self):
-
-        deck = list(range(0, 10))
-
-        sequence = ['cut 6','deal with increment 7', 'deal into new stack']
-
-        expected_shuffle = [3,0,7,4,1,8,5,2,9,6]
-        actual_shuffle = shuffle(deck, sequence)
-        self.assertEqual(actual_shuffle, expected_shuffle)
-
-    def test_given_three(self):
-
-        deck = list(range(0, 10))
-
-        sequence = ['deal with increment 7', 'deal with increment 9', 'cut -2']
-
-        expected_shuffle = [6,3,0,7,4,1,8,5,2,9]
-        actual_shuffle = shuffle(deck, sequence)
-        self.assertEqual(actual_shuffle, expected_shuffle)
-
-    def test_given_four(self):
-
-        deck = list(range(0, 10))
-
-        sequence = ['deal into new stack', 'cut -2', 'deal with increment 7', 'cut 8', 'cut -4', 'deal with increment 7', 'cut 3', 'deal with increment 9', 'deal with increment 3', 'cut -1']
-
-        expected_shuffle = [9,2,5,8,1,4,7,0,3,6]
-        actual_shuffle = shuffle(deck, sequence)
-        self.assertEqual(actual_shuffle, expected_shuffle)
-
     def test_part_1_actual_input(self):
 
-        deck = list(range(0, 10007))
+        deck = 10007
 
         sequence = ['deal with increment 74','deal into new stack','deal with increment 67','cut 6315','deal with increment 15','cut -8049','deal with increment 69','cut 2275','deal with increment 25','cut 4811','deal with increment 47','cut -9792','deal with increment 26','cut -3014','deal with increment 47','cut -1093','deal with increment 39','cut -5322','deal with increment 14','cut -7375','deal with increment 16','cut 9627','deal into new stack','cut 1632','deal into new stack','cut -2904','deal with increment 69','cut -3328','deal with increment 60','cut 7795','deal into new stack','deal with increment 37','cut -4238','deal with increment 19','cut -3170','deal with increment 45','cut 8631','deal with increment 64','cut -2380','deal with increment 59','cut -2802','deal with increment 19','cut -3369','deal with increment 45','deal into new stack','deal with increment 71','cut 5452','deal with increment 73','cut -6609','deal with increment 33','cut 1892','deal with increment 5','cut 1395','deal into new stack','cut -8514','deal with increment 46','deal into new stack','deal with increment 15','cut 3963','deal with increment 2','cut -2965','deal into new stack','cut 640','deal with increment 13','cut 8889','deal with increment 62','cut 8331','deal with increment 49','cut 6169','deal with increment 71','deal into new stack','deal with increment 33','cut 6342','deal with increment 52','cut 2875','deal with increment 39','cut 4283','deal with increment 19','cut 4102','deal with increment 57','deal into new stack','cut -7801','deal with increment 38','cut 4273','deal with increment 58','cut -2971','deal with increment 46','deal into new stack','cut 8043','deal with increment 52','cut -7108','deal with increment 21','cut 507','deal with increment 70','cut -8658','deal with increment 64','cut 7213','deal into new stack','deal with increment 61','cut 9439',]
 
         expected_position_of_2019 = 3939
 
-        shuffled_deck = shuffle(deck, sequence)
-        actual_position_of_2019 = shuffled_deck.index(2019)
+        actual_pair = shuffle(deck, sequence)
 
-        self.assertEqual(actual_position_of_2019, expected_position_of_2019)
+        current_pos = actual_pair[0]
+        i = 0
 
+        while (current_pos != 2019):
+            i += 1
+            current_pos += actual_pair[1]
+            current_pos %= deck
+
+        self.assertEqual(i, expected_position_of_2019)
+        
 
 if __name__ == '__main__':
     unittest.main()
